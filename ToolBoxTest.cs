125a0605dc65ad8384385e63d4f11e9ae9c12e08
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2020-05-16 16:26:18 +0500

    Update ToolBoxTest.cs

--- ToolBoxTest.cs
+++ ToolBoxTest.cs
@@ -1,19 +1,11 @@
 ﻿using Sirenix.OdinInspector;
-using Sirenix.Serialization;
-using ToolBox.Modules;
-using ToolBox.Serialization;
+using ToolBox.Pools;
 using UnityEngine;
 
 namespace ToolBox.Test
 {
 	public class ToolBoxTest : SerializedMonoBehaviour
 	{
-		[OdinSerialize] private ModulesContainer modulesContainer = default;
 
-		private void Awake()
-		{
-			DataSerializer.CreateFile(0, false);
-			DataSerializer.LoadFile();
-		}
 	}
 }

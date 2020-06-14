3cd2595ccfc357d0cb17accba383bd046512058a
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2020-06-14 15:54:42 +0500

    Update ToolBox.cs

--- ToolBox.cs
+++ ToolBox.cs
@@ -16,62 +16,6 @@ namespace ToolBox.Extensions
 		}
 		#endregion
 
-		#region GameObject Extensions
-		public static void ResetTag(this GameObject obj) =>
-			obj.tag = "Untagged";
-
-		public static void AddTag(this GameObject entity, Tag tag) =>
-			tag.Add(entity.GetHashCode());
-
-		public static void RemoveTag(this GameObject entity, Tag tag) =>
-			tag.Remove(entity.GetHashCode());
-
-		public static bool HasTag(this GameObject entity, Tag tag) =>
-			tag.HasEntity(entity.GetHashCode());
-
-		public static void AddTags(this GameObject entity, Tag[] tags)
-		{
-			int hash = entity.GetHashCode();
-
-			for (int i = 0; i < tags.Length; i++)
-				tags[i].Add(hash);
-		}
-
-		public static void RemoveTags(this GameObject entity, Tag[] tags)
-		{
-			int hash = entity.GetHashCode();
-
-			for (int i = 0; i < tags.Length; i++)
-				tags[i].Remove(hash);
-		}
-
-		public static bool HasTags(this GameObject entity, Tag[] tags, bool allRequired)
-		{
-			int hash = entity.GetHashCode();
-
-			if (allRequired)
-			{
-				for (int i = 0; i < tags.Length; i++)
-				{
-					if (!tags[i].HasEntity(hash))
-						return false;
-				}
-
-				return true;
-			}
-			else
-			{
-				for (int i = 0; i < tags.Length; i++)
-				{
-					if (tags[i].HasEntity(hash))
-						return true;
-				}
-
-				return false;
-			}
-		}
-		#endregion
-
 		#region Vector3 Extensions
 		public static Vector3 Multiply(this Vector3 vector, float multiplier)
 		{

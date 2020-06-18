0d56c8637a95426831204c06a71c61cc1ab80d16
Vladislav Kinyashov <indraayy322@gmail.com>
2020-06-18 17:59:54 +0500

    Update TypeGeneration.cs

--- Editor/TypeGeneration.cs
+++ Editor/TypeGeneration.cs
@@ -10,16 +10,16 @@
 
 				base.SetData();
 
-				_folders[0] = "Assets/ToolBox/Signals/Signals/Generics";
-				_templates[0] = "Assets/ToolBox/Main/Editor/Templates/SignalTemplate.cs.txt";
+				_folders[0] = "Assets/[1]ToolBox/Signals/Signals/Generics";
+				_templates[0] = "Assets/[1]ToolBox/Main/Editor/Templates/SignalTemplate.cs.txt";
 				_names[0] = $"{_fileName}Signal";
 
-				_folders[1] = "Assets/ToolBox/Signals/Receivers/Generics";
-				_templates[1] = "Assets/ToolBox/Main/Editor/Templates/ReceiverTemplate.cs.txt";
+				_folders[1] = "Assets/[1]ToolBox/Signals/Receivers/Generics";
+				_templates[1] = "Assets/[1]ToolBox/Main/Editor/Templates/ReceiverTemplate.cs.txt";
 				_names[1] = $"{_fileName}Receiver";
 
-				_folders[2] = "Assets/ToolBox/Reactors/Generics";
-				_templates[2] = "Assets/ToolBox/Main/Editor/Templates/ReactorHelperTemplate.cs.txt";
+				_folders[2] = "Assets/[1]ToolBox/Reactors/Generics";
+				_templates[2] = "Assets/[1]ToolBox/Main/Editor/Templates/ReactorHelperTemplate.cs.txt";
 				_names[2] = $"{_fileName}Helper";
 			}
 		}

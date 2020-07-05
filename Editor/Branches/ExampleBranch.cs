39c1ef52740d6da8c66e3f46febd08ff29131201
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2020-07-05 16:43:07 +0500

    upd

---
+++ Editor/Branches/ExampleBranch.cs
@@ -0,0 +1,17 @@
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using ToolBox.Localization;
+using UnityEngine;
+
+namespace ToolBox.Editor
+{
+	public class ExampleBranch : IBranch
+	{
+		public string Path => "Example";
+
+		public void Setup()
+		{
+			
+		}
+	}
+}

69daaea7ccb37af342847334974dae11f7bfe05e
Vladislav Kinyashov <indraayy322@gmail.com>
2020-12-07 14:56:06 +0500

    Delete TagCreatorBranch.cs

--- Editor/Branches/TagCreatorBranch.cs
+++
@@ -1,9 +0,0 @@
-﻿using ToolBox.Tags;
-
-namespace ToolBox.Editor
-{
-	public class TagCreatorBranch : ScriptableObjectCreator<Tag>, IBranch
-	{
-		public string Path => "ToolBox/Creators/Tag Creator";
-	}
-}

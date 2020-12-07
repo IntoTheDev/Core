8f3873be27e87c56c9950e311ee5cd80e2a47631
Vladislav Kinyashov <indraayy322@gmail.com>
2020-12-07 14:56:30 +0500

    Update ScriptableObjectCreator.cs

--- Editor/Branches/ScriptableObjectCreator.cs
+++ Editor/Branches/ScriptableObjectCreator.cs
@@ -1,37 +1,57 @@
-﻿using Sirenix.OdinInspector;
+using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 namespace ToolBox.Editor
 {
-	public abstract class ScriptableObjectCreator<T> where T : ScriptableObject
+	public class ScriptableObjectCreator : IBranch
 	{
+		[ShowInInspector, ValueDropdown(nameof(GetAssetTypes)), OnValueChanged(nameof(OnTypeChanged))] private Type _assetType = null;
 		[SerializeField] private string _assetName = "Asset";
-		[SerializeField, FolderPath, OnValueChanged(nameof(OnPathChange))] private string _path = "";
+		[SerializeField, FolderPath(RequireExistingPath = true), OnValueChanged(nameof(OnPathChange))] private string _path = "";
+		[ShowInInspector, InlineEditor(Expanded = true)] private ScriptableObject _asset = null;
 
 		private string _saveKey = "";
 
-		public virtual void Setup(OdinMenuTree tree)
-		{
-			_saveKey = $"Creator{GetType()}";
-			_path = EditorPrefs.GetString(_saveKey, "");
-		}
+		public string Path => "ToolBox/Scriptable Object Creator";
+
+		public virtual void Setup(OdinMenuTree tree) { }
+
+		private IEnumerable<Type> GetAssetTypes() =>
+			AssemblyUtilities.GetTypes(AssemblyTypeFlags.UserTypes)
+				.Where(x => x.InheritsFrom<ScriptableObject>() && !x.IsAbstract);
 
 		[Button(ButtonSizes.Medium)]
 		private void CreateAsset()
 		{
-			var asset = ScriptableObject.CreateInstance<T>();
-			asset.name = _assetName;
-			string path = $"{_path}/{_assetName}.asset";
-			path = AssetDatabase.GenerateUniqueAssetPath(path);
-			AssetDatabase.CreateAsset(asset, path);
+			if (_assetType == null)
+				return;
+
+			_asset.name = _assetName;
+			string path = AssetDatabase.GenerateUniqueAssetPath($"{_path}/{_assetName}.asset");
+
+			AssetDatabase.CreateAsset(_asset, path);
 			AssetDatabase.SaveAssets();
 			AssetDatabase.Refresh();
 
 			EditorUtility.FocusProjectWindow();
 
-			Selection.activeObject = asset;
+			Selection.activeObject = _asset;
+		}
+
+		private void OnTypeChanged()
+		{
+			if (_asset != null)
+				UnityEngine.Object.DestroyImmediate(_asset);
+
+			_asset = ScriptableObject.CreateInstance(_assetType);
+			_saveKey = $"AssetCreator{_assetType.Name}";
+			_path = EditorPrefs.GetString(_saveKey, "");
 		}
 
 		private void OnPathChange() =>

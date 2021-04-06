5885ff9fedbb86907a73b0fe40bf71eeb3fd1a30
Vladislav Kinyashov <indraayy322@gmail.com>
2021-04-06 20:27:48 +0500

    Update Helper.cs

--- Runtime/Helper.cs
+++ Runtime/Helper.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -179,19 +180,17 @@ namespace ToolBox.Runtime.Extensions
 			return closest;
 		}
 
+		// TODO: Move to separate file
 #if UNITY_EDITOR
 		public static List<T> GetAllAssetsOfType<T>() where T : Object
 		{
+			var paths = AssetDatabase.GetAllAssetPaths();
 			var assets = new List<T>();
-			string[] guids = AssetDatabase.FindAssets($"t:{typeof(T)}");
 
-			for (int i = 0; i < guids.Length; i++)
+			foreach (var path in paths)
 			{
-				string assetPath = AssetDatabase.GUIDToAssetPath(guids[i]);
-				var asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
-
-				if (asset != null)
-					assets.Add(asset);
+				var asset = AssetDatabase.LoadAssetAtPath<T>(path);
+				assets.Add(asset);
 			}
 
 			return assets;

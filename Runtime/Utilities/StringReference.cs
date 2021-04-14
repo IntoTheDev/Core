65d8ec2f7eec8344e4fa6c1d1622818731f0e85f
Vladislav Kinyashov <indraayy322@gmail.com>
2021-04-14 21:48:51 +0500

    Update StringReference.cs

--- Runtime/Utilities/StringReference.cs
+++ Runtime/Utilities/StringReference.cs
@@ -1,14 +1,19 @@
-﻿#if ODIN_INSPECTOR
+#if ODIN_INSPECTOR
 using Sirenix.OdinInspector;
 #endif
-using ToolBox.Runtime.Attributes;
 using UnityEngine;
 
 namespace ToolBox.Runtime.Utilities
 {
-	[CreateAssetMenu(menuName = "ToolBox/String Reference"), ShowInMenu]
+#if ODIN_INSPECTOR
+	[Required, AssetSelector, InlineEditor(InlineEditorModes.GUIOnly, Expanded = true)]
+#endif
+	[CreateAssetMenu(menuName = "ToolBox/String Reference")]
 	public sealed class StringReference : ScriptableObject
 	{
+#if ODIN_INSPECTOR
+		[DisableInInlineEditors]
+#endif
 		[SerializeField] private string _value = string.Empty;
 
 		public string Value => _value;

d2f3be01abbc248076a40867f09aa39764f7a2f5
Vladislav Kinyashov <indraayy322@gmail.com>
2021-04-14 15:00:28 +0500

    Update SerializedDictionary.cs

--- Runtime/Lookups/SerializedDictionary.cs
+++ Runtime/Lookups/SerializedDictionary.cs
@@ -6,31 +6,34 @@ using System.Collections.Generic;
 #endif
 using UnityEngine;
 
-[Serializable]
-public class SerializedDictionary<K, V> : Dictionary<K, V>, ISerializationCallbackReceiver
+namespace ToolBox.Runtime.Lookups
 {
-	[SerializeField] private List<K> _keys = new List<K>();
-	[SerializeField] private List<V> _values = new List<V>();
-
-	public void OnBeforeSerialize()
+	[Serializable]
+	public class SerializedDictionary<K, V> : Dictionary<K, V>, ISerializationCallbackReceiver
 	{
-		_keys.Clear();
-		_values.Clear();
-		using var enumerator = GetEnumerator();
-		while (enumerator.MoveNext())
+		[SerializeField] private List<K> _keys = new List<K>();
+		[SerializeField] private List<V> _values = new List<V>();
+
+		public void OnBeforeSerialize()
 		{
-			var current = enumerator.Current;
-			_keys.Add(current.Key);
-			_values.Add(current.Value);
+			_keys.Clear();
+			_values.Clear();
+			using var enumerator = GetEnumerator();
+			while (enumerator.MoveNext())
+			{
+				var current = enumerator.Current;
+				_keys.Add(current.Key);
+				_values.Add(current.Value);
+			}
 		}
-	}
 
-	public void OnAfterDeserialize()
-	{
-		for (int i = 0; i < _keys.Count; i++)
-			Add(_keys[i], _values[i]);
+		public void OnAfterDeserialize()
+		{
+			for (int i = 0; i < _keys.Count; i++)
+				Add(_keys[i], _values[i]);
 
-		_keys.Clear();
-		_values.Clear();
+			_keys.Clear();
+			_values.Clear();
+		}
 	}
 }

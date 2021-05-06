704f655f0ead106e2bc4394be4ad56f904e89bf6
Vladislav Kinyashov <indraayy322@gmail.com>
2021-05-06 15:01:46 +0500

    Update FixedStopwatch.cs

--- Runtime/Utilities/FixedStopwatch.cs
+++ Runtime/Utilities/FixedStopwatch.cs
@@ -1,6 +1,6 @@
 ﻿namespace ToolBox.Runtime.Utilities
 {
-	public class FixedStopwatch : BaseStopwatch
+	public sealed class FixedStopwatch : BaseStopwatch
 	{
 		protected override float Time => UnityEngine.Time.fixedTime;
 

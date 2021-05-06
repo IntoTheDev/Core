89e0892e892def81d736427f5771aa6c35477b19
Vladislav Kinyashov <indraayy322@gmail.com>
2021-05-06 17:38:23 +0500

    Update BaseStopwatch.cs

--- Runtime/Utilities/BaseStopwatch.cs
+++ Runtime/Utilities/BaseStopwatch.cs
@@ -4,31 +4,31 @@ namespace ToolBox.Runtime.Utilities
 	{
 		private readonly float _duration = 0f;
 		private float _timestamp = 0f;
-		private bool _isEllapsed = false;
+		private bool _isElapsed = false;
 
 		protected abstract float Time { get; }
 
-		public bool IsEllapsed 
+		public bool IsElapsed 
 		{
 			get
 			{
 				// Preventing from using UnityEngine.Time which is quiet expensive performance wise
-				if (_isEllapsed)
+				if (_isElapsed)
 					return true;
 
-				_isEllapsed = Time - _timestamp > _duration;
+				_isElapsed = Time - _timestamp > _duration;
 
-				return _isEllapsed;
+				return _isElapsed;
 			}
 		}
-		
-		public BaseStopwatch(float duration) =>
-			_duration = duration;
 
-		public void Start()
+		public void Reset()
 		{
 			_timestamp = Time;
-			_isEllapsed = false;
+			_isElapsed = false;
 		}
+
+		protected BaseStopwatch(float duration) =>
+			_duration = duration;
 	}
 }
